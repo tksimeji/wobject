966aa5bd6f5517303721de9b8f2e13b7faf25e04
tksimeji <tksimeji@outlook.com>
2025-01-15 02:13:41 +0900

    0.1.0: release

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.tksimeji"
-version = "0.0.0"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
--- src/main/java/com/tksimeji/wobject/Wobject.java
+++ src/main/java/com/tksimeji/wobject/Wobject.java
@@ -9,16 +9,24 @@ import com.tksimeji.wobject.listener.ServerListener;
 import com.tksimeji.wobject.reflect.WobjectClass;
 import com.tksimeji.wobject.runnable.ItemStackRunnable;
 import com.tksimeji.wobject.util.ResourceUtility;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.UUID;
 
 public final class Wobject extends JavaPlugin {
+    private static final Logger log = LoggerFactory.getLogger(Wobject.class);
     private static Wobject instance;
 
     private static final @NotNull WobjectLoader loader = new WobjectLoader();
@@ -26,6 +34,15 @@ public final class Wobject extends JavaPlugin {
     public static @NotNull Wobject plugin() {
         return instance;
     }
+
+    public static @NotNull String version() {
+        return plugin().getPluginMeta().getVersion();
+    }
+
+    private static @NotNull ComponentLogger logger() {
+        return plugin().getComponentLogger();
+    }
+
 
     public static @Nullable Object get(@Nullable UUID uuid) {
         return all().stream()
@@ -95,5 +112,12 @@ public final class Wobject extends JavaPlugin {
         new ItemStackRunnable().runTaskTimerAsynchronously(this, 0L, 16L);
 
         ResourceUtility.newResource("wobject.json", false);
+
+        logger().info(Component.text(" __      _____  ").color(TextColor.color(17, 154, 142)));
+        logger().info(Component.text(" \\ \\ /\\ / / _ \\ ").color(TextColor.color(31, 183, 136)).append(Component.text("    Wobject - " + version()).color(NamedTextColor.WHITE)));
+        logger().info(Component.text("  \\ V  V / (_) |").color(TextColor.color(43, 210, 131)));
+        logger().info(Component.text("   \\_/\\_/ \\___/ ").color(TextColor.color(56, 239, 125)).append(Component.text("    Help poor children in Uganda!").color(NamedTextColor.GRAY)));
+        logger().info(Component.text("                ").append(Component.text("    ").append(Component.text("https://iccf-holland.org/").color(NamedTextColor.BLUE).decorate(TextDecoration.UNDERLINED))));
+        logger().info(Component.empty());
     }
 }
